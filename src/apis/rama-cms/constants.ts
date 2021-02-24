ac320fc45a3b73b3f13e10991adc8c65676cbfb8
arthur.jardim@sequor.com.br <arthur.jardim@sequor.com.br>
2021-02-24 11:00:00 -0300

    atualizacao

--- src/apis/rama-cms/constants.ts
+++ src/apis/rama-cms/constants.ts
@@ -1,2 +1,2 @@
-// export const base_url = 'https://rama-cms.herokuapp.com'
-export const base_url = 'http://localhost:1337'
+export const base_url = 'https://rama-cms.herokuapp.com'
+// export const base_url = 'http://localhost:1337'
