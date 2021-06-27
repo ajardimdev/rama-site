41b9ad3dac0c3f998b5d6426e377ca5f25aab1d1
Arthur Jardim <arthur.campao@cwi.com.br>
2021-06-27 01:29:47 -0300

    adicionando google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Document, { DocumentInitialProps, Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
+import { GA_TRACKING_ID } from '../lib/gtag'
+
 export default class MyDocument extends Document {
   static async getInitialProps(ctx): Promise<DocumentInitialProps> {
     const sheet = new ServerStyleSheet()
@@ -39,6 +41,15 @@ export default class MyDocument extends Document {
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Cantarell&family=Fira+Sans&family=Oxygen:wght@300;400;700&family=Roboto&family=Ubuntu&display=swap" rel="stylesheet" />
                 <link rel="icon" href="/R_P.png" />
+
+                <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+                <script dangerouslySetInnerHTML={{ __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_TRACKING_ID}', {
+                    page_path: window.location.pathname,
+                  });`}} />
             </Head>
             <body>
                 <Main />
