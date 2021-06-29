e8c787d495681d5581de251af8af4f0f68bdc2e0
Arthur Jardim <arthur.campao@cwi.com.br>
2021-06-29 13:23:00 -0300

    add blobstoraage

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withImages = require('next-images')
 module.exports = withImages({
     esModule: true,
     images: {
-        domains: ['rama-cms.herokuapp.com'],
+        domains: ['rama-cms.herokuapp.com', 'ramablob.blob.core.windows.net'],
         deviceSizes: [640, 750, 828, 1080, 1920, 2048, 3840]
     }
 })
